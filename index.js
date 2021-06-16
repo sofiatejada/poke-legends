b197036ae84e465c2e4b693c11ce62700cb65d77
Sofia Tejada <sofianais@icloud.com>
2021-06-15 17:56:55 -0700

    add booleans to data

--- index.js
+++ index.js
@@ -8,50 +8,14 @@ const port = 4852;
 
 //our first ENDPOINT / ROUTE
 //node will listen for requests at the home path: '/'
-// app.get('/some-route', (req, res) => {
-    //when it receives a request at this path, it will respond....
-    //with hello world!
-//   res.send('Smello World!')
+app.get('/pokelegends', (req, res) => {
+  res.json(legends)
 });
-// app.get('/other-route', (req, res) => {
-    //when it receives a request at this path, it will respond....
-    //with hello world!
-//   res.send('Other thing! By the way!!!')
-// });
 
-//wait for me to make requests to this
-
-// app.get('/pokemon', (req, res) => {
-    
-//     const legends = {
-
-//     };
-
-
-//     res.json(legends);
-// });
-
-// app.get('/2/', (req, res) => {
-    
-//     const legends = {
-
-//     };
-
-//     const id = 2;
-
-//     const pokemon2 = legends.results.find((singleLegendaryPoke) => singleLegendaryPoke.id === id);
-
-
-//     res.json(pokemon2);
-// });
-
-app.get('/legends/:id', (req, res) => {
-    const legends = {
-
-    };
+app.get('/pokelegends/:id', (req, res) => {
     const id = Number(req.params.id);
 
-    const pokemon = legends.results.find((singlePokemon) => singlePokemon.id === id);
+    const pokemon = legends.pokemon.find((singlePokemon) => singlePokemon.id === id);
 
     res.json(pokemon);
 });
