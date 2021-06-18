6ec2707bd7d27f2091e3e4d799f2ced339138ae4
Sofia Tejada <sofianais@icloud.com>
2021-06-18 10:10:14 -0700

    change some stuff in files around

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 //servers send responses and receive requests (from clients/front-end)
 //imports in node look like this!
 //one of the onlyr eal differences in syntax between node and javascript
-const express = require('express');
-const legends = require('./data.js');
-const app = express();
+const app = require('./pokelegends.js');
 const port = process.env.PORT || 3000;
-
-//our first ENDPOINT / ROUTE
-//node will listen for requests at the home path: '/'
-app.get('/pokelegends', (req, res) => {
-  res.json(legends)
-});
-
-app.get('/pokelegends/:id', (req, res) => {
-    const id = Number(req.params.id);
-
-    const pokemon = legends.pokemon.find((singlePokemon) => singlePokemon.id === id);
-
-    res.json(pokemon);
-});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
